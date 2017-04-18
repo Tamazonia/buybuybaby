cbe2556dee43f778a12c7e71f4f5854d48305499
Arthur Ramos <ramos.oceano@gmail.com>
2017-04-18 13:30:53 -0300

    added new category to clothing

--- app/helpers/clothings_helper.rb
+++ app/helpers/clothings_helper.rb
@@ -1,2 +1,9 @@
 module ClothingsHelper
+  def clothing_pic(clothing)
+    if clothing.photo? == false
+      asset_path('boy_clothing.jpeg')
+    else
+      cl_image_path clothing.photo, height: 300, width: 400, crop: :fill
+    end
+  end
 end
