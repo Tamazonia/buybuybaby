356cbe3e5151e9076b78e4297612682e014da105
pedro almeida <pedro.marc2003@gmail.com>
2017-04-20 17:08:03 -0300

    itens comprados

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,23 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @clothings = @user.clothings
 
+    # 1a forma -> traz apenas o que tem
+
+    #@purchases = Purchase.where(clothing_id: @clothings.pluck(:id))
+
+    # 2a forma -> traz apenas o que tem
+
+    #@purchases = []
+    #@clothings.each do |clothing|
+      #@purchases << clothing.purchase
+    #end
+
+    # 3a forma -> traz tb nil
+    #@purchases = @clothings.map(&:purchase)
+    # 3a forma upgrade -> traz apenas o que tem
+    @purchases = @clothings.map(&:purchase).compact
+
+
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -20,11 +20,11 @@ ActiveRecord::Schema.define(version: 20170420175538) do
     t.string   "age"
     t.string   "description"
     t.integer  "user_id"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "gender"
     t.string   "photo"
-    t.boolean  "bought",      default: false
+    t.boolean  "bought"
     t.index ["user_id"], name: "index_clothings_on_user_id", using: :btree
   end
 
